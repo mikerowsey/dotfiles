1469d9933e394276db6255a6129f92b715276543
Michael Rowsey <michael@rowsey.org>
2026-02-24 10:23:37 -0800

    add keybind to nvim to see hidden in telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
       telescope.setup({})
       local map = vim.keymap.set
       map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+      map("n", "<leader>fa", "<cmd>Telescope find_files hidden=true<cr>", { desc = "Find all files (incl. dotfiles)" })
       map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Grep" })
       map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Buffers" })
     end,
