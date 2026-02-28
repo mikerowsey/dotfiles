2ba5c0a1ca2c0c4c411c6da9ac715b48ad89a954
Michael Rowsey <michael@rowsey.org>
2026-02-28 14:56:31 -0800

    wip

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,47 +1,79 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "L3MON4D3/LuaSnip",
-        },
-        config = function()
-            local cmp = require("cmp")
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			-- Optional but useful if you want command-line completion later:
+			-- "hrsh7th/cmp-cmdline",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local compare = require("cmp.config.compare")
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<CR>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.confirm({ select = false })
-                        else
-                            fallback()
-                        end
-                    end),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                }),
-                sources = {
-                    { name = "nvim_lsp" },
-                },
-            })
-        end,
-    },
+			cmp.setup({
+				completion = {
+					completeopt = "menu,menuone,noselect",
+				},
+
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+
+					-- Confirm only when you explicitly select something (no surprise accepts)
+					["<CR>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.confirm({ select = false })
+						else
+							fallback()
+						end
+					end),
+
+					-- Tab cycles only when the menu is visible; otherwise it's a real tab/indent
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<Esc>"] = cmp.mapping.abort(),
+				}),
+
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+					{ name = "buffer" },
+				}),
+
+				sorting = {
+					priority_weight = 2,
+					comparators = {
+						compare.offset,
+						compare.exact,
+						compare.score,
+						compare.recently_used,
+						compare.kind,
+						compare.sort_text,
+						compare.length,
+						compare.order,
+					},
+				},
+
+				experimental = {
+					ghost_text = true,
+				},
+			})
+		end,
+	},
 }
