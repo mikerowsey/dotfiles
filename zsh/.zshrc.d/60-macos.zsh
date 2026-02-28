243b6a5ea2cd37e8df1fb7e56a4477c92afebeab
Michael Rowsey <michael@rowsey.org>
2026-02-28 09:38:26 -0800

    add homebrew clang to macos paths

--- zsh/.zshrc.d/60-macos.zsh
+++ zsh/.zshrc.d/60-macos.zsh
@@ -7,4 +7,10 @@ alias o='open'
 alias clipcopy='pbcopy'
 alias clippaste='pbpaste'
 
+# Homebrew LLVM toolchain
 export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export CC="/opt/homebrew/opt/llvm/bin/clang"
+export CXX="/opt/homebrew/opt/llvm/bin/clang++"
+export AR="/opt/homebrew/opt/llvm/bin/llvm-ar"
+export NM="/opt/homebrew/opt/llvm/bin/llvm-nm"
+export RANLIB="/opt/homebrew/opt/llvm/bin/llvm-ranlib"
