3c0744d4772920d51241e9e98fd7d3de0475289b
Michael Rowsey <mike@rowsey.com>
2026-06-18 17:00:26 -0700

    add insstall script, change master to main

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cp user-home/.bash_profile ~/
+cp user-home/.bashrc ~/
+cp user-home/.gitconfig ~/
+cp user-home/.gitignore_global ~/
+cp user-home/.gitattributes ~/
+cp user-home/.tmux.conf ~/
+cp user-home/.vimrc ~/
